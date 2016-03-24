93ce907542dc2a01dc42407228ec9625e0442a85
Nelson <nelsont87@gmail.com>
2016-03-24 09:18:55 -0700

    Included canvas to page

--- script.js
+++ script.js
@@ -27,40 +27,43 @@ $('#no').click(function() {
 
 
 
-
-
-//variable game to put the puppy at risk of not being saved
-
 // var userChoice = prompt("Do you choose rock, paper or scissors?");
 // var computerChoice = Math.random();
 // if (computerChoice < 0.34) {
-//     computerChoice = "rock";
+//   computerChoice = "rock";
 // } else if(computerChoice <= 0.67) {
-//     computerChoice = "paper";
+//   computerChoice = "paper";
 // } else {
-//     computerChoice = "scissors";
-// }
+//   computerChoice = "scissors";
+// } console.log("Computer: " + computerChoice);
 
-// if (choice1 === "paper") {
-//     if (choice2 === "rock") {
-//         return "paper wins";
-//     } else {
-//         if (choice2 === "scissors") {
-//             return "scissors wins";
+// var compare=function(choice1,choice2) {
+//     if (choice1===choice2) {
+//         return"The result is a tie!";
+//     }
+//     else if (choice1==="rock") {
+//         if (choice2==="scissors") {
+//             return"rock wins";
+//         }else {
+//             return"paper wins";
 //         }
 //     }
-//     if (choice1 === "scissors") {
-//         if (choice2 === "rock") {
-//             return "rock wins";
+//     else if (choice1==="paper") {
+//         if (choice2==="rock") {
+//             return"paper wins";
 //         } else {
-//             if (choice2 === "paper") {
-//                 return "scissors wins";
-//             }
+//             return"scissors wins";
+//         }
+//     }
+//     else {
+//         if (choice2==="rock") {
+//             return"rock wins";
+//         } else {
+//             return"scissors wins";
 //         }
 //     }
 // }
-
-
+// compare(userChoice,computerChoice)
 
 
 
@@ -122,9 +125,41 @@ function initAutocomplete() {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
       } else {
-        bounds.extend(place.geometry.location);
-      }
-      
+        bounds.extend(place.geometry.location);     
+    }
+    	function Platform(pos, wid, hei){
+  this.draw = function(ctx){
+    ctx.strokeRect(pos.x+.5, pos.y+.5, 20, 20);
+  };
+}
+
+var can = document.getElementById("can"),
+    ctx = can.getContext('2d'),
+    wid = can.width,
+    hei = can.height,
+    numPlatforms = 14,
+    platWid = 20,
+    platHei = 20,
+    platforms = [],
+    hash = {};
+
+for(var i = 0; i < numPlatforms; i++){
+  var posX = Math.floor(Math.random()*(wid-platWid)/platWid)*platWid,
+    posY = Math.floor(Math.random()*(hei-platHei)/platHei)*platHei;
+  
+  while (hash[posX + 'x' + posY]){
+    posX = Math.floor(Math.random()*wid/platWid)*platWid;
+    posY = Math.floor(Math.random()*hei/platHei)*platHei;
+  }
+  
+  hash[posX + 'x' + posY] = 1; 
+  platforms.push(new Platform({x:posX, y:posY}, platWid, platHei));
+}
+
+for(var i = 0; i < platforms.length; i++){
+  platforms[i].draw(ctx);
+}
+
     });
     map.fitBounds(bounds);
     });
